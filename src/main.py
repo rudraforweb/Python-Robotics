31bc9a894729cd7e56df8fb2a6398c1ac140caa4
Rudra Kumar <rkumar_365@outlook.com>
2023-10-19 21:07:55 -0400

    Added basic varibles.

--- src/main.py
+++ src/main.py
@@ -12,4 +12,10 @@ from vex import *
 
 # Brain should be defined by default
 brain=Brain()
+brain_inertial = Inertial()
+motorone=Motor(Ports.PORT7, True)
+motortwo=Motor(Ports.PORT12, True)
+claw_motor = Motor(Ports.PORT4, True)
+
+
 
