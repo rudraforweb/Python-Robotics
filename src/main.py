7dbeaeeb546cdfab278c1bdd855c23ba8c8e5cf7
Rudra Kumar <rkumar_365@outlook.com>
2023-10-21 11:11:31 -0400

    Added functions for ease of use.

--- src/main.py
+++ src/main.py
@@ -40,26 +40,59 @@ brain.screen.next_row()
 brain.screen.print("Touchled good!")
 wait(0.2, SECONDS)
 brain.screen.next_row()
-colorsensor = ColorSensor(Ports.PORT11)
+color = ColorSensor(Ports.PORT11)
+color.set_light(100, PERCENT)
 brain.screen.print("Color sensor good!")
 wait(0.2, SECONDS)
 brain.screen.next_row()
-brain.screen.print("All good!")
+distance = Distance(Ports.PORT8)
+brain.screen.print("Distance sensor good!")
+wait(0.2, SECONDS)
+brain.screen.next_row()
+brain.battery.capacity()
+brain.screen.print("Battery good!")
+wait(0.2, SECONDS)
 touchled.set_color(Color.GREEN)
 wait(1, SECONDS)
 brain.screen.clear_screen()
 brain.screen.set_cursor(2, 2)
 brain.screen.set_font(FontType.PROP30)
-brain.screen.print(" Python-Robotics")
+brain.screen.print("Python-Robotics")
 #End of "system check".
+
+#Define functions:
+def left_turn():
+    drivetrain.turn_for(LEFT, 90, DEGREES)
+
+def right_turn():
+    drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+def end_code():
+    touchled.set_color(Color.RED)
+    drivetrain.stop()
+
+def place_holder_text():
+    brain.screen.clear_screen()
+    brain.screen.set_cursor(2, 2)
+    brain.screen.print("Python-Robotics")
+    
+def off_edge():
+    if True:
+        if distance.object_distance(INCHES) > 1:
+            brain.screen.clear_screen()
+            brain.screen.set_cursor(2, 2)
+            brain.screen.print("Turning...")
+            drivetrain.drive_for(REVERSE, 200, MM, wait=True)
+            right_turn()
+        else:
+            drivetrain.drive(FORWARD)
+            place_holder_text()   
 
 #Add color to Touchled:
 touchled.set_color(Color.BLUE)
+
 #Claw Motor ready position:
 claw_motor.spin_to_position(-40, DEGREES)
 
-
-
-
-
+#Main code:
 
