4161a58da7edd4470ded5e444f8c5d8a5732d4bd
Rudra Kumar <rkumar_365@outlook.com>
2023-10-20 23:12:46 -0400

    Added drivetrain and other things.

--- src/main.py
+++ src/main.py
@@ -10,25 +10,54 @@
 # Library imports
 from vex import *
 
-# Brain should be defined by default
+# Start of 'system check'.
 brain=Brain()
-brain.screen.print("Program started!")
+touchled = Touchled(Ports.PORT9)
+touchled.set_color(Color.RED)
+brain.screen.set_font(FontType.MONO12)
+brain.screen.print("Running system check:")
 brain.screen.next_row()
-brain.screen.print("Define: Brain defined")
+wait(0.2, SECONDS)
+brain.screen.print("Brain good!")
 brain.screen.next_row()
+wait(0.2, SECONDS)
 brain_inertial = Inertial()
-brain.screen.print("Define: Inertial defined")
+brain_inertial.calibrate()
+brain.screen.print("Inertial good!")
+wait(0.2, SECONDS)
 brain.screen.next_row()
-leftmotor=Motor(Ports.PORT7, True)
-brain.screen.print("Define: Left motor defined")
-brain.screen.next_row()
+leftmotor=Motor(Ports.PORT7, False)
 rightmotor=Motor(Ports.PORT12, True)
-brain.screen.print("Define: Right motor defined")
+drivetrain=DriveTrain(leftmotor, rightmotor)
+brain.screen.print("Drivetrain good!")
+wait(0.2, SECONDS)
 brain.screen.next_row()
 claw_motor = Motor(Ports.PORT10, True)
-brain.screen.print("Define: Claw motor defined")
-wait(5)
+claw_motor.set_max_torque(50, PERCENT)
+brain.screen.print("Claw motor good!")
+wait(0.2, SECONDS)
+brain.screen.next_row()
+brain.screen.print("Touchled good!")
+wait(0.2, SECONDS)
+brain.screen.next_row()
+colorsensor = ColorSensor(Ports.PORT11)
+brain.screen.print("Color sensor good!")
+wait(0.2, SECONDS)
+brain.screen.next_row()
+brain.screen.print("All good!")
+wait(0.5, SECONDS)
 brain.screen.clear_screen()
+brain.screen.set_cursor(2, 2)
+brain.screen.set_font(FontType.PROP30)
+brain.screen.print(" Python-Robotics")
+#End of "system check".
+
+#Add color to Touchled:
+touchled.set_color(Color.BLUE)
+claw_motor.spin_to_position(-40, DEGREES)
+
+
+
 
 
 
