0c8559652b69f2b2c8737acbf98bc6778d5d75e3
Rudra Kumar <rkumar_365@outlook.com>
2023-10-20 21:38:06 -0400

    Added "next row" to avoid having all text in one row.

--- src/main.py
+++ src/main.py
@@ -13,15 +13,21 @@ from vex import *
 # Brain should be defined by default
 brain=Brain()
 brain.screen.print("Program started!")
+brain.screen.next_row()
 brain.screen.print("Define: Brain defined")
+brain.screen.next_row()
 brain_inertial = Inertial()
 brain.screen.print("Define: Inertial defined")
+brain.screen.next_row()
 leftmotor=Motor(Ports.PORT7, True)
 brain.screen.print("Define: Left motor defined")
+brain.screen.next_row()
 rightmotor=Motor(Ports.PORT12, True)
 brain.screen.print("Define: Right motor defined")
+brain.screen.next_row()
 claw_motor = Motor(Ports.PORT10, True)
 brain.screen.print("Define: Claw motor defined")
+brain.screen.clear_screen()
 
 
 
